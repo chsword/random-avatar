c5a8eb4ee4e4e369206466d9b7460014aac97d9e
ZouJian <chsword@126.com>
2015-11-20 02:33:43 +0800

    png

--- source/RandomAvator.Core/RandomAvatarException.cs
+++ source/RandomAvator.Core/RandomAvatarException.cs
@@ -2,6 +2,8 @@
 
 namespace RandomAvator
 {
+    //http://stackoverflow.com/questions/27917258/system-drawing-namespace-missing-in-aspnetvnext-core-clr/27964081#27964081
+    //http://stackoverflow.com/questions/28427607/which-package-do-i-need-for-images-in-asp-net-5/28427930#28427930
     public class RandomAvatarException : Exception
     {
         public RandomAvatarException(Exception ex) : base("", ex)
